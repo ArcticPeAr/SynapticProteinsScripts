65dee43208f943c3751dabfbce4554e5e75017c7
ArcticPeAr <peterarnesen@protonmail.com>
2024-03-07 21:16:52 +0100

    R and ggplot2 used to match papers design by BE

--- PathwayBarplots.R
+++ PathwayBarplots.R
@@ -6,17 +6,28 @@ df <- read.csv('pathway_data.csv')
 #remove the columns index, column 8 and column 9
 df <- df[, -c(1, 8, 9)]
 
+# remove rows with p-value > 0.01
+df <- df[df$Adjusted.p.value < 0.01,]
+
+# add three spaces to the beginning of each pathway name
+df$Pathway <- paste0(" ", df$Pathway)
+
 # create barplot where the x-axis is the pathway and the y-axis is the adjusted p-value
 
-pdf("pathway_barplot.pdf", width = 10, height = 10)
+pdf("pathway_barplot.pdf", width = 15, height = 10)
 
 ggplot(df, aes(x = reorder(Pathway, -Adjusted.p.value), y = Adjusted.p.value)) +
   geom_bar(stat = "identity", fill = "#01c0b1") +
   coord_flip() +  
   theme_minimal() +  
-  geom_text(aes(label = Pathway, y = 0), hjust = -0.1, angle = 0, color = "black") +
+  geom_text(aes(label = Pathway, y = 0), hjust = 0.0, vjust = 0.8, angle = 0, color = "black", position = position_identity()) +
   labs(x = "Pathway", y = "Adjusted p-value") +
-  theme(axis.text.y = element_blank(), 
-        axis.ticks.y = element_blank()) 
+  theme(
+    axis.text.y = element_blank(), 
+    axis.ticks.y = element_blank(),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank()
+  )
 
 dev.off()
+
